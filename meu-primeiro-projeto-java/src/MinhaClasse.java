b859a6c75840b511f7a0e6cec6c88fd65b6d8eb2
Fernando Barbosa <ofernandobarbosa@gmail.com>
2022-08-17 16:44:46 -0300

    aprendendo sobre classes

---
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -0,0 +1,16 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println();
+
+        String nome = "Fernando";
+        String sobrenome = "Barbosa";
+
+        String nomeCompleto = nomeCompleto(nome, sobrenome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
